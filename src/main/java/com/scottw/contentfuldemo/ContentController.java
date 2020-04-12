58b792ede529b1b2d46f32638d22de95f32e1cbc
Scott Weaver <scott@weaverclan.org>
2020-04-12 17:09:23 -0500

    added access to data by fields

--- src/main/java/com/scottw/contentfuldemo/ContentController.java
+++ src/main/java/com/scottw/contentfuldemo/ContentController.java
@@ -1,5 +1,10 @@
 package com.scottw.contentfuldemo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -59,13 +64,150 @@ public class ContentController {
 			//Doing this for now, as I don't know if you can unwrap an asset
 			for(CDAAsset image: product.images) {
 				System.out.println("image fields = " + image.id() + ", " + image.mimeType() + ", " + image.title() + ", " + image.url() + "\n\r");
-				output = output + "image fields = " + image.id() + ", " + image.mimeType() + ", " + image.title() + ", " + image.url() + "\n\r" + "<br><br>";
+				output = output + "image fields = " + image.id() + ", " + image.mimeType() + ", " + image.title() + ", " + image.url() + "<br><br>";
 			}
 
 //			System.out.println("product details - id = " + entryId);
 //			System.out.println(product.productDescription + "\n\r");
 			
 		}
+		return output;
+	}
+
+	@RequestMapping(value = "/contentful-demo/products2", method = RequestMethod.GET)
+	public String getProducts2() {
+
+		CDAClient client = getClient();
+		
+		// Fetch entries
+		CDAArray array = 
+		    client
+		        .fetch(CDAEntry.class)
+		        .withContentType("product")
+		        .include(1)
+		        .all();
+		
+//		array.items().forEach((item) -> System.out.println(item.attrs().toString()));
+
+	
+		String output = "";
+		
+		Set entryKeys = array.entries().keySet();
+		Collection<CDAEntry> entryValues = array.entries().values();
+
+		Set assetKeys = array.assets().keySet();
+		Collection<CDAAsset> assetValues = array.assets().values();
+		
+		System.out.println("CDAEntry Collection keys = " + entryKeys.toString());
+		System.out.println("CDAEntry Collection values = " + entryValues.toString());
+		
+		CDAEntry entry = (CDAEntry) entryValues.toArray()[0];
+		System.out.println("rawFields = " + entry.rawFields().toString());
+		
+		output = "Hi from product2() - " + entryValues.size() + "entries<br><br>";
+		
+		
+		for(CDAEntry item : entryValues) {
+			String entryId = item.id();
+			
+			System.out.println("item attrs = " + item.attrs().toString());
+
+			//System.out.println("rawFields = " + item.rawFields().toString());
+			if(item.contentType().id().equals("product")) {
+				output = output + item.getField("productName") + "<br>";
+			}
+			if(item.contentType().id().equals("brand")) {
+				output = output + item.getField("companyName") + "<br>";
+			}
+			if(item.contentType().id().equals("category")) {
+				output = output + item.getField("title") + "<br>";
+			}
+			output = output + "contentType id = " + item.contentType().id() + ", name = " + item.contentType().name() + "<br><br>";
+			//Object obj = (Object) item.rawFields().get("productName");
+			// Get a null pointer exception if the commented out code is run.
+			//output = output + obj.getClass() + "<br><br>";
+			//output = output + item.rawFields().toString() + "<br><br>";
+
+			for(String key: item.rawFields().keySet()) {
+				output = output + key + " = " + item.getField(key) + "<br>";
+				output = output + " className = " + item.getField(key).getClass().toString() + "<br>";
+			}
+			
+			output = output + "**************************************************************************<br><br>";
+/*			Product product = client
+				    .observeAndTransform(Product.class)
+				    .one(entryId)
+				    .blockingSingle();
+		
+
+			System.out.println(item.attrs().toString()+"\n\r");
+			System.out.println(product.toString()+"\n\r");
+
+			output = output + product.toString() + "<br><br>";
+			
+			//Doing this for now, as I don't know if you can unwrap an asset
+			for(image: product.images) {
+				System.out.println("image fields = " + image.id() + ", " + image.mimeType() + ", " + image.title() + ", " + image.url() + "\n\r");
+				output = output + "image fields = " + image.id() + ", " + image.mimeType() + ", " + image.title() + ", " + image.url() + "<br><br>";
+			}
+
+//			System.out.println("product details - id = " + entryId);
+//			System.out.println(product.productDescription + "\n\r");
+			
+*/		}
+		
+
+		for(CDAAsset item : assetValues) {
+			String assetId = item.id();
+			
+			System.out.println("asset attrs = " + item.attrs().toString());
+
+			//System.out.println("rawFields = " + item.rawFields().toString());
+			output = output + item.type().toString() + "<br><br>";
+/*			
+			if(item.type(). .contentType().id().equals("product")) {
+				output = output + item.getField("productName") + "<br>";
+			}
+			if(item.contentType().id().equals("brand")) {
+				output = output + item.getField("companyName") + "<br>";
+			}
+			if(item.contentType().id().equals("category")) {
+				output = output + item.getField("title") + "<br>";
+			}
+			output = output + "contentType id = " + item.contentType().id() + ", name = " + item.contentType().name() + "<br><br>";
+*/			//Object obj = (Object) item.rawFields().get("productName");
+			// Get a null pointer exception if the commented out code is run.
+			//output = output + obj.getClass() + "<br><br>";
+			//output = output + item.rawFields().toString() + "<br><br>";
+
+			for(String key: item.rawFields().keySet()) {
+				output = output + key + " = " + item.getField(key) + "<br>";
+				output = output + " className = " + item.getField(key).getClass().toString() + "<br>";
+			}
+			
+			output = output + "**************************************************************************<br><br>";
+/*			Product product = client
+				    .observeAndTransform(Product.class)
+				    .one(entryId)
+				    .blockingSingle();
+		
+
+			System.out.println(item.attrs().toString()+"\n\r");
+			System.out.println(product.toString()+"\n\r");
+
+			output = output + product.toString() + "<br><br>";
+			
+			//Doing this for now, as I don't know if you can unwrap an asset
+			for(image: product.images) {
+				System.out.println("image fields = " + image.id() + ", " + image.mimeType() + ", " + image.title() + ", " + image.url() + "\n\r");
+				output = output + "image fields = " + image.id() + ", " + image.mimeType() + ", " + image.title() + ", " + image.url() + "<br><br>";
+			}
+
+//			System.out.println("product details - id = " + entryId);
+//			System.out.println(product.productDescription + "\n\r");
+			
+*/		}
+
 		return output;
 	}
 	@RequestMapping(value = "/contentful-demo/categories", method = RequestMethod.GET)
@@ -140,7 +282,15 @@ public class ContentController {
 				    .one(entryId)
 				    .blockingSingle();
 		
+/*			ArrayList<Brand> brands = client
+				    .observeAndTransform(Brand.class)
+				    .all()
+				    .;
+//				    .blockingSingle();
 
+			System.out.println("all brands \n\r");
+			System.out.println(brands.toString());
+*/			
 			System.out.println(item.attrs().toString()+"\n\r");
 			System.out.println(brand.toString()+"\n\r");
 
